238f075eb6f5f52f6ebfa445e5b312760e46e516
Yifeng Wang <git@yifengwang.me>
2019-01-25 10:44:10 -0800

    add solution for 3.1 measurable interface

--- src/ch3/Ch3_1.java
+++ src/ch3/Ch3_1.java
@@ -1,11 +1,36 @@
-package ch3;
-
 public class Ch3_1 {
-    static public void main() {
+    public static void main(String[] args) {
+        Measurable[] measurables = {
+                new Employee(1),
+                new Employee(2),
+                new Employee(3),
+        };
 
+        System.out.println(average(measurables));
+    }
+
+    static double average (Measurable[] measurables) {
+        double sum = 0;
+        for (Measurable measurable : measurables) {
+            sum += measurable.getMeasure();
+        }
+        return sum / measurables.length;
     }
 }
 
 interface Measurable {
     double getMeasure();
+}
+
+class Employee implements Measurable {
+    private double salary;
+
+    public Employee(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public double getMeasure () {
+        return this.salary;
+    }
 }
