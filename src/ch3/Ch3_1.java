068b41aef40bd2d851e555234df0d2d32c3d160e
Yifeng Wang <git@yifengwang.me>
2019-01-25 11:05:36 -0800

    add 3.2 largest

--- src/ch3/Ch3_1.java
+++ src/ch3/Ch3_1.java
@@ -6,31 +6,68 @@ public class Ch3_1 {
                 new Employee(3),
         };
 
-        System.out.println(average(measurables));
-    }
+        System.out.println(Measurable.average(measurables));
 
-    static double average (Measurable[] measurables) {
+        Employee[] employees = {
+                new Employee(1, "John"),
+                new Employee(2, "Burr"),
+                new Employee(3, "Alex"),
+        };
+
+        System.out.println(Employee.getLargest(employees));
+    }
+}
+
+interface Measurable {
+    double getMeasure();
+
+    static double average(Measurable[] measurables) {
         double sum = 0;
         for (Measurable measurable : measurables) {
             sum += measurable.getMeasure();
         }
         return sum / measurables.length;
     }
-}
 
-interface Measurable {
-    double getMeasure();
+    static Measurable largest(Measurable[] measurables) {
+        Measurable largest = measurables[0];
+
+        for (Measurable measurable : measurables) {
+            if (measurable.getMeasure() > largest.getMeasure()) {
+                largest = measurable;
+            }
+        }
+
+        return largest;
+    }
 }
 
 class Employee implements Measurable {
     private double salary;
+    private String name = "no name";
 
     public Employee(double salary) {
         this.salary = salary;
     }
 
+    public Employee(double salary, String name) {
+        this(salary);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     @Override
-    public double getMeasure () {
+    public double getMeasure() {
         return this.salary;
+    }
+
+    public static String getLargest(Employee[] employees) {
+        Measurable largest = Measurable.largest(employees);
+        Employee largestEmployee = (Employee) largest;
+
+        return largestEmployee.getName();
     }
 }
