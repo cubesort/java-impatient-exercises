bdb43bcb4875fc0af1d99f48e6489d947d1255c4
Yifeng Wang <git@yifengwang.me>
2019-01-25 16:57:00 -0800

    scaffold ch3_4

---
+++ src/ch3/Ch3_4.java
@@ -0,0 +1,16 @@
+public class Ch3_4 {
+    public static void main(String[] args) {
+
+    }
+}
+
+interface Sequnce {
+    
+    boolean hasNext();
+}
+
+class IntSequence {
+    static IntSequence of (Double[] numbers) {
+
+    }
+}
