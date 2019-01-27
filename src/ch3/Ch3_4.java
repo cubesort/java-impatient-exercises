9cc62592664e24ae47027c492f340260f07734c9
Yifeng Wang <git@yifengwang.me>
2019-01-26 16:16:46 -0800

    add 3.4 IntSequence.of

--- src/ch3/Ch3_4.java
+++ src/ch3/Ch3_4.java
@@ -1,16 +1,30 @@
+package ch3;
+
 public class Ch3_4 {
     public static void main(String[] args) {
-
+        IntSequence seq = IntSequence.of(3, 1, 4, 1, 5, 6);
+        System.out.println(seq.hasNext());
+        System.out.println(seq.next());
     }
 }
 
-interface Sequnce {
-    
+interface IntSequence {
+    int next();
+
     boolean hasNext();
-}
 
-class IntSequence {
-    static IntSequence of (Double[] numbers) {
+    static IntSequence of (int... numbers) {
+        return new IntSequence() {
+            private int counter = 0;
 
+            public boolean hasNext() {
+                return counter < numbers.length;
+            }
+
+            public int next() {
+                counter++;
+                return numbers[counter - 1];
+            }
+        };
     }
 }
