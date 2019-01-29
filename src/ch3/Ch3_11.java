d1bef4583b06c2c8ef2e11392d3b0a2c24ba2d49
Yifeng Wang <git@yifengwang.me>
2019-01-28 21:10:15 -0800

    add 3.11 list subdirectory

--- src/ch3/Ch3_11.java
+++ src/ch3/Ch3_11.java
@@ -1,13 +1,33 @@
 package ch3;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
 
 public class Ch3_11 {
     static public void main(String[] args) {
-
+        System.out.println(Arrays.toString(ls("./src")));
+        System.out.println(Arrays.toString(lsMethodExpression("./src")));
+        System.out.println(Arrays.toString(lsAnonymousClass("./src")));
     }
 
-    static File[] ls() {
-        
+    static File[] ls(String pathname) {
+        File file = new File(pathname);
+        return file.listFiles(f -> f.isDirectory());
+    }
+
+    static File[] lsMethodExpression(String pathname) {
+        File file = new File(pathname);
+        return file.listFiles(File::isDirectory);
+    }
+
+    static File[] lsAnonymousClass(String pathname) {
+        File file = new File(pathname);
+        return file.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.isDirectory();
+            }
+        });
     }
 }
