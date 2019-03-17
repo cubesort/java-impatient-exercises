50a67fee5b6ddd198d83f66383d74bd28f583089
Yifeng Wang <git@yifengwang.me>
2019-03-16 20:57:54 -0700

    add 7.7 word count

---
+++ src/ch7/Ch7_7.java
@@ -0,0 +1,33 @@
+package ch7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Ch7_7 {
+    public static void main(String[] args) {
+        try {
+            String path = System.getProperty("java.class.path");
+            Map<String, Integer> counts = count(path + "/ch7/words.txt");
+            System.out.println(counts);
+        } catch(FileNotFoundException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+
+    public static Map<String, Integer> count(String path) throws FileNotFoundException {
+        try (Scanner in = new Scanner(new File(path))) {
+            in.useDelimiter(" ");
+            Map<String, Integer> counts = new TreeMap<>();
+
+            while (in.hasNext()) {
+                String word = in.next();
+                counts.compute(word, (k, v) -> v == null ? 1 : v + 1);
+            }
+
+            return counts;
+        }
+    }
+}
