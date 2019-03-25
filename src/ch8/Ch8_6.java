fe45e53e68235e8d59ea81bb900e2879cdcbc626
Yifeng Wang <git@yifengwang.me>
2019-03-25 14:50:00 -0700

    add 8.6 isWord

---
+++ src/ch8/Ch8_6.java
@@ -0,0 +1,14 @@
+package ch8;
+
+public class Ch8_6 {
+    public static void main(String[] args) {
+        boolean isWord1 = isWord("word");
+        boolean isWord2 = isWord("word2");
+        System.out.println(isWord1);
+        System.out.println(isWord2);
+    }
+
+    public static boolean isWord(String str) {
+        return str.codePoints().anyMatch(Character::isAlphabetic);
+    }
+}
