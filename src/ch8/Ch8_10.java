c4695ce39412a51fc79cec1729c99b31c794bac3
Yifeng Wang <git@yifengwang.me>
2019-03-27 21:50:32 -0700

    fix exercise numbers

--- src/ch8/Ch8_10.java
+++ src/ch8/Ch8_10.java
@@ -1,42 +1,24 @@
 package ch8;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Ch8_10 {
-    private static String[] VOWELS = { "a", "e", "i", "o", "u" };
-
     public static void main(String[] args) {
         try {
-            System.out.println(Arrays.toString(wordsWithAllVowels("resources/words.txt")));
+            Stream<String> words = Stream.of(Ch8_7.first100words("resources/words.txt"));
+            System.out.println(getAverageLength(words));
+
         } catch (IOException ex) {
-            System.out.println(ex.toString());
+            System.out.println(ex.getMessage());
         }
     }
 
-    private static String[] wordsWithAllVowels(String pathname) throws IOException {
-        Path path = Path.of(System.getProperty("user.dir"), pathname);
-
-        try (Scanner in = new Scanner(path)) {
-            return in
-                    .useDelimiter("[^\\w]")
-                    .tokens()
-                    .filter(Ch8_10::hasAllVowels)
-                    .toArray(String[]::new);
-        }
-    }
-
-    private static boolean hasAllVowels(String str) {
-        String lower = str.toLowerCase();
-
-        for (String vowel : VOWELS) {
-            if (!lower.contains(vowel)) {
-                return false;
-            }
-        }
-
-        return true;
+    public static double getAverageLength(Stream<String> words) {
+        return words.collect(Collectors.summarizingInt(String::length)).getAverage();
     }
 }
---
+++ src/ch8/Ch8_9.java
@@ -0,0 +1,42 @@
+package ch8;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ch8_9 {
+    private static String[] VOWELS = { "a", "e", "i", "o", "u" };
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(Arrays.toString(wordsWithAllVowels("resources/words.txt")));
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+
+    private static String[] wordsWithAllVowels(String pathname) throws IOException {
+        Path path = Path.of(System.getProperty("user.dir"), pathname);
+
+        try (Scanner in = new Scanner(path)) {
+            return in
+                    .useDelimiter("[^\\w]")
+                    .tokens()
+                    .filter(Ch8_9::hasAllVowels)
+                    .toArray(String[]::new);
+        }
+    }
+
+    private static boolean hasAllVowels(String str) {
+        String lower = str.toLowerCase();
+
+        for (String vowel : VOWELS) {
+            if (!lower.contains(vowel)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
