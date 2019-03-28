cc1d44888b403af285bf5516a0d297e05f0b011c
Yifeng Wang <git@yifengwang.me>
2019-03-27 21:04:10 -0700

    clean up code

--- src/ch8/Ch8_7.java
+++ src/ch8/Ch8_7.java
@@ -19,7 +19,7 @@ public class Ch8_7 {
         Path path = Path.of(System.getProperty("user.dir"), pathname);
         try (Scanner in = new Scanner(path)) {
             return in
-                    .useDelimiter("[[^\\w]]")
+                    .useDelimiter("[^\\w]")
                     .tokens()
                     .filter(Ch8_6::isWord)
                     .limit(100)
