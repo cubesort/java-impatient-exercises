6d0bfd00f4e28978293edf06cd7f13d4b7e3b4bb
Yifeng Wang <git@yifengwang.me>
2019-03-27 21:42:06 -0700

    add 8.11 find average length of words

---
+++ src/ch8/Ch8_11.java
@@ -0,0 +1,24 @@
+package ch8;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ch8_11 {
+    public static void main(String[] args) {
+        try {
+            Stream<String> words = Stream.of(Ch8_7.first100words("resources/words.txt"));
+            System.out.println(getAverageLength(words));
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static double getAverageLength(Stream<String> words) {
+        return words.collect(Collectors.summarizingInt(String::length)).getAverage();
+    }
+}
